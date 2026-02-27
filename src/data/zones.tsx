3850dc28f643f06331f3df65626bb2a668c96d45
Nik Steel <nikolassteel@gmail.com>
2026-02-26 22:21:10 -0500

    update image paths

--- src/data/zones.tsx
+++ src/data/zones.tsx
@@ -15,21 +15,21 @@ export const _zonePages : ZoneProps[] = [
     {
         name: "Woodworking",
         slug: "woodworking",
-        image: "/images/zones/woodworking.jpg",
+        image: "images/zones/woodworking.jpg",
         benefits: [
             {
                 title: "Sawstop1",
-                image: "/images/zones/benefits/sawstop.jpg",
+                image: "images/zones/benefits/sawstop.jpg",
                 description: "...1"
             },
             {
                 title: "Sawstop2",
-                image: "/images/zones/benefits/sawstop.jpg",
+                image: "images/zones/benefits/sawstop.jpg",
                 description: "...2"
             },
             {
                 title: "Sawstop3",
-                image: "/images/zones/benefits/sawstop.jpg",
+                image: "images/zones/benefits/sawstop.jpg",
                 description: "...3"
             }
         ]
@@ -37,41 +37,41 @@ export const _zonePages : ZoneProps[] = [
     {
         name: "Laser Cutting",
         slug: "laser-cutting",
-        image: "/images/zones/laser-cutting.jpg"
+        image: "images/zones/laser-cutting.jpg"
     },
     {
         name: "CAD/CAM",
         slug: "cad-cam",
-        image: "/images/zones/cad-cam.jpg"
+        image: "images/zones/cad-cam.jpg"
     },
     {
         name: "CNC Cutting",
         slug: "cnc",
-        image: "/images/zones/cnc.jpg"
+        image: "images/zones/cnc.jpg"
     },
     {
         name: "Electronics",
         slug: "electronics",
-        image: "/images/zones/electronics.jpg"
+        image: "images/zones/electronics.jpg"
     },
     {
         name: "Fibre Arts",
         slug: "fibre-arts",
-        image: "/images/zones/fibre-arts.jpg"
+        image: "images/zones/fibre-arts.jpg"
     },
     {
         name: "Machining",
         slug: "machining",
-        image: "/images/zones/machining.jpg"
+        image: "images/zones/machining.jpg"
     },
     {
         name: "Stained Glass",
         slug: "stained-glass",
-        image: "/images/zones/stained-glass.jpg"
+        image: "images/zones/stained-glass.jpg"
     },
     {
         name: "3D Printing",
         slug: "3d-printing",
-        image: "/images/zones/3d-printing.jpg"
+        image: "images/zones/3d-printing.jpg"
     }
 ];
