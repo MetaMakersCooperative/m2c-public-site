41dbc485732f7f9aabb4181675d601591d1f8274
Nik Steel <nikolassteel@gmail.com>
2026-02-26 22:15:39 -0500

    fix image paths on gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  basePath: isProd ? '/m2c-public-site' : '',
+  assetPrefix: isProd ? '/m2c-public-site/' : '',
 };
 
 export default nextConfig;
