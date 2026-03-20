25da3ed17a849bebc0ab131185f01e4a04137dae
Nik Steel <nikolassteel@gmail.com>
2026-03-19 22:10:05 -0400

    fix header offset on zeffy embed

--- src/app/(common)/zeffy-campaign.jsx
+++ src/app/(common)/zeffy-campaign.jsx
@@ -3,7 +3,7 @@ import { HEADER_HEIGHT } from "@/data/navigation";
 
 export default function ZeffyCampaign({campaignId}) {
     return (
-        <Box position="relative" overflow="hidden" width="100%" height={`calc(100vh - ${HEADER_HEIGHT})`} marginTop={`${HEADER_HEIGHT}px`}>
+        <Box position="relative" overflow="hidden" width="100%" top={HEADER_HEIGHT} height={`calc(100vh - ${HEADER_HEIGHT})`}>
             <iframe title='Donation form powered by Zeffy' 
                 style={{position: "absolute", border: 0, top:0, left:0, bottom:0, right:0, width: "100%", height: "100%"}}
                 src={`https://www.zeffy.com/embed/ticketing/${campaignId}`}
