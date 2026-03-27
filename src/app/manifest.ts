ca671b632de335dde9b67adaf7ec06d46fa9f1e2
Nik Steel <nikolassteel@gmail.com>
2026-03-26 21:21:56 -0400

    update favicons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,12 +14,14 @@ export default function manifest(): MetadataRoute.Manifest {
             {
                 "src": "/logos/icon-192.png",
                 "sizes": "192x192",
-                "type": "image/png"
+                "type": "image/png",
+                "purpose": "maskable"
             },
             {
                 "src": "/logos/icon-512.png",
                 "sizes": "512x512",
-                "type": "image/png"
+                "type": "image/png",
+                "purpose": "maskable"
             },
         ],
     }
